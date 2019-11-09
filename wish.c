6c76001ac5f23d21f0ece8c620f4e9bb3db9392e
mikkomajamaa <mikko.majamaa@outlook.com>
2019-11-09 11:22:47 +0200

    add parse command and arguments

--- wish.c
+++ wish.c
@@ -4,7 +4,14 @@
 
 int main(int argc, char *argv[]) {
   char *line = NULL;
+  char *argument;
+  char *path = "/bin";
   size_t len = 0;
+
+  //
+  char *str1, *str2, *token, *subtoken;
+  char *saveptr1, *saveptr2;
+  int i, j;
 
   // wrong number of arguments
   if (argc > 2) {
@@ -19,6 +26,29 @@ int main(int argc, char *argv[]) {
     }
 
     // TODO: handle commands (and arguments)
+    for (i = 1, str1 = line; ; i++, str1 = NULL) {
+      token = strtok_r(str1, "\n", &saveptr1);
+      if (token == NULL) {
+        break;
+      }
+      //printf("%d: %s\n", i, token);
+
+      for (str2 = token, j = 0; ;str2 = NULL, j++) {
+        subtoken = strtok_r(str2, " ", &saveptr2);
+
+        if (subtoken == NULL) {
+          break;
+        }
+
+        if (j == 0) {
+          printf("exec %s with arguments: ", subtoken);
+        } else {
+          printf("%s ", subtoken);
+        }
+
+      }
+      printf("\n");
+    }
 
     //
 
